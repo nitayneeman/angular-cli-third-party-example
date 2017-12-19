ce31a56c618090ace0745f4e19de2267fdd5ef2f
nitayneeman <nitayneeman@users.noreply.github.com>
2017-12-19 22:22:14 +0200

    Added a sample for lodash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'How to Add Third-Party Library in Angular CLI';
+export class AppComponent implements OnInit {
+  public title = 'How to Add Third-Party Library in Angular CLI';
+
+  ngOnInit() {
+    console.log('Is the title empty?', this.isEmpty(this.title));
+  }
+
+  private isEmpty(value: any): boolean {
+    return _.isEmpty(value);
+  }
 }
